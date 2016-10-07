344eb5c390e15a4bd62cd666a59a11de1824e1d9
james tak <jtak93@gmail.com>
2016-10-07 16:44:28 -0700

    adds connection log

--- config/database.js
+++ config/database.js
@@ -2,4 +2,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = Promise;
 mongoose.connect(`${process.env.DB_URI}`);
 
+mongoose.connection.once('open', function() {
+  console.log(`You're connected to: ${con.host}:${con.port}`)
+});
+
 module.exports = mongoose;
