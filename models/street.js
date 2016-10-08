8ec372df35bed78bf2a38b54a8561e156bdd9602
james tak <jtak93@gmail.com>
2016-10-08 14:27:58 -0700

    adds cross street coordinate validation and module.exports

--- models/street.js
+++ models/street.js
@@ -1,19 +1,47 @@
 var mongoose = require('mongoose');
 
 var commentSchema = new mongoose.Schema({
-  body: {type: String, required: true}
+  body: {
+      type: String,
+      required: true
+  },
   commentBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User'
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now
   }
-  createdAt: {type: Date, default: Date.now}
 });
 
 
 var streetSchema = new mongoose.Schema({
   address: String,
-  crossStreets: String,
+  crossStreetsCoordinates: {
+    type: [
+        {
+          lat: String,
+          lng: String
+        }
+    ],
+    validate:[arrayLimit];
+  }
   comments: [commentSchema],
-  createdAt: {type: Date, default: Date.now},
-  updatedAt: {type: Date, default: Date.now}
+  createdAt: {
+      type: Date,
+      default: Date.now
+  },
+  updatedAt: {
+      type: Date,
+      default: Date.now
+  }
 });
+
+function arrayLimit(val) {
+  return val.length <= 2;
+
+module.exports = mongoose.model('Street', streetSchema);
+
+
+
