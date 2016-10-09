45b5221523513e800cd752b0df9fee8956abce9f
Winnie Luk <winniecluk@gmail.com>
2016-10-08 19:55:11 -0700

    make basic map display

---
+++ public/javascripts/app.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var myLocation = {lat: 34, lng: -118};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLocation
+  });
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: map
+  });
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { apiKey: process.env.MAPS_KEY });
 });
 
 module.exports = router;
