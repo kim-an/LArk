b9b703a5fcaecfcbd02240756ff17e47643f8d14
Winnie Luk <winniecluk@gmail.com>
2016-10-08 20:24:52 -0700

    make small zoom change

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 function initMap() {
   var myLocation = {lat: 34, lng: -118};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
+    zoom: 18,
     center: myLocation
   });
   var marker = new google.maps.Marker({
