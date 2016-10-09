26fa0c3658df957ef6b51f0816b5defebaac54e6
Winnie Luk <winniecluk@gmail.com>
2016-10-09 14:03:01 -0700

    install and mount cors

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var cors = require('cors');
 
 require('dotenv').config();
 require('./config/database');
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
