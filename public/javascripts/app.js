1ee5c17d822094e70ba7d03772a15d8ec4f53fef
james tak <jtak93@gmail.com>
2016-10-09 19:29:18 -0700

    loops through fake db to place markers

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -7,30 +7,27 @@ $(document).ready(function(){
 // fake db
 var places = [
   {
-    "latitude": 35,
-    "longitude": -119
-  },
-  {
     "latitude": 34.030935,
     "longitude": -118.26663732
+  },
+  {
+    "latitude": 34.0305689,
+    "longitude": -118.2663225
   }
 ];
 
 
 function initMap() {
+  // TODO: need to dynamically change myLocation using location services
   var myLocation = {lat: 34.030935, lng: -118.266637};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
     center: myLocation
   });
-  var marker = new google.maps.Marker({
-    position: myLocation,
-    map: map
-  });
   // loop through fake db and mark each place
   places.forEach(function(place){
     console.log(place);
-    var latLng = new google.maps.LatLng(places.latitude, places.longitude);
+    var latLng = {lat: place.latitude, lng: place.longitude};
     var marker = new google.maps.Marker({
       position: latLng,
       map: map
@@ -50,6 +47,8 @@ function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
     // use results[0].geometry.location.lat(), results[0].geometry.location.lng() to get lat/long
+    console.log(results[0].geometry.location.lat());
+    console.log(results[0].geometry.location.lng());
     if (status === 'OK') {
       resultsMap.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
