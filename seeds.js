782e42451166039ee6432e39ca681a7397d9426d
james tak <jtak93@gmail.com>
2016-10-10 18:05:37 -0700

    initial seed tip data

--- seeds.js
+++ seeds.js
@@ -1,22 +1,78 @@
 require('dotenv').config();
 require('./config/database');
 var User = require('./models/user');
-var Street = require('./models/street');
+var Tip = require('./models/tip');
 
-User.remove({}, function() {
-  Street.remove({}, function() {
-    var street = new Street({
-      address: '1900 S Broadway, Los Angeles',
-      coordinates: {lat: '34.0305689', lng: '-118.2663225'},
-      crossStreetsCoordinates: [
-        {lat: '34.031446', lng: '-118.265743'},
-        {lat: '34.029429', lng: ' -118.267120'}
-      ]
-    });
-    street.save(function(err) {
-      console.log(err);
-      console.log(street);
-      process.exit();
-    });
+
+
+Promise.all([
+  Tip.remove({})
+]).then(function() {
+  var tip1 = new Tip({
+    userID: "57fc315af5eca32f92259e4a",
+    parkingType: "Street",
+    coordinates: {lat: 34.0305689, lng: -118.2663225},
+    comment: {message: "This street is whack"},
+    validHours: [
+      {day: 'Sun', startTime: '8am', endTime: '6pm'},
+      {day: 'Mon', startTime: '8am', endTime: '6pm'},
+      {day: 'Tue', startTime: '8am', endTime: '6pm'},
+      {day: 'Wed', startTime: '8am', endTime: '6pm'},
+      {day: 'Thu', startTime: '8am', endTime: '6pm'},
+      {day: 'Fri', startTime: '8am', endTime: '6pm'},
+      {day: 'Sat', startTime: '8am', endTime: '6pm'}
+    ],
+    permit: false,
+    cost: '$1/hr',
+    costExceptions: [],
+    maxTime: '4hrs'
+  });
+  tip1.save().then(function() {
+  });
+}).then(function() {
+  var tip2 = new Tip({
+    userID: "57fc315af5eca32f92259e4a",
+    parkingType: "Street",
+    coordinates: {lat: 34.031094, lng: -118.267396},
+    comment: {message: "This street is Hill st with cross streets: 21st and Washington"},
+    validHours: [
+      {day: 'Sun', startTime: '8am', endTime: '6pm'},
+      {day: 'Mon', startTime: '8am', endTime: '6pm'},
+      {day: 'Tue', startTime: '8am', endTime: '6pm'},
+      {day: 'Wed', startTime: '8am', endTime: '6pm'},
+      {day: 'Thu', startTime: '8am', endTime: '6pm'},
+      {day: 'Fri', startTime: '8am', endTime: '6pm'},
+      {day: 'Sat', startTime: '8am', endTime: '6pm'}
+    ],
+    permit: false,
+    cost: '$1/hr',
+    costExceptions: [],
+    maxTime: '4hrs'
+  });
+  tip2.save().then(function() {
+  });
+}).then(function() {
+  var tip3 = new Tip({
+    userID: "57fc315af5eca32f92259e4a",
+    parkingType: "Outdoor Lot",
+    coordinates: {lat: 34.030231, lng: -118.266989},
+    comment: {message: "This is the Reef parking lot"},
+    validHours: [
+      {day: 'Sun', startTime: '5am', endTime: '11pm'},
+      {day: 'Mon', startTime: '5am', endTime: '11pm'},
+      {day: 'Tue', startTime: '5am', endTime: '11pm'},
+      {day: 'Wed', startTime: '5am', endTime: '11pm'},
+      {day: 'Thu', startTime: '5am', endTime: '11pm'},
+      {day: 'Fri', startTime: '5am', endTime: '11pm'},
+      {day: 'Sat', startTime: '5am', endTime: '11pm'}
+    ],
+    permit: false,
+    cost: '$10/day',
+    costExceptions: ['$5 after 4pm']
+  });
+  tip3.save().then(function() {
+    process.exit();
   });
 });
+
+
