dfc103c490355755dfe3c52b5b0a368304297fac
Sochin Ancheta <sochink@gmail.com>
2016-10-13 14:41:05 -0700

    created center button on map & added favicon

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -56,6 +56,39 @@ function render(tips) {
   });
 }
 
+// for center button on map
+function CenterControl(controlDiv, map) {
+
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to recenter the map';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = 'Center Map';
+        controlUI.appendChild(controlText);
+
+        // Setup the click event listeners.
+        controlUI.addEventListener('click', function() {
+          map.setCenter(myLocation);
+        });
+
+      }
+
 function initMap() {
   // styled map
   var styledMapType = new google.maps.StyledMapType([
@@ -94,6 +127,14 @@ function initMap() {
     zoom: 18,
     center: startLocation
   });
+
+  // for center button on map /////////////////////
+    var centerControlDiv = document.createElement('div');
+    var centerControl = new CenterControl(centerControlDiv, map);
+
+    centerControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(centerControlDiv);
+
 
   // associating the styled map w/existing map
   map.mapTypes.set('styled_map', styledMapType);
